4c27e4457a9b11e0482321de938df6739fc40518
Alonso Barrios <39711667+alonso1311@users.noreply.github.com>
2019-06-02 22:44:13 -0500

    Add files via upload

--- uniform-interface-master/web/server.py
+++ uniform-interface-master/web/server.py
@@ -73,10 +73,95 @@ def update_user():
 
 #Delete
 @app.route('/users', methods = ['DELETE'])
-def delete_message():
+def delete_user():
     id = request.form['key']
     session = db.getSession(engine)
     messages = session.query(entities.User).filter(entities.User.id == id)
+    for message in messages:
+        session.delete(message)
+    session.commit()
+    return "Deleted User"
+
+"""@app.route('/authenticate', methods=["POST"])
+def authenticate():
+    #1. Get data from request
+    username = request.form['username'];
+    password = request.form['password'];
+
+    #2. look in database
+    db_session = db.getSession(engine)
+    try:
+        user = db_session.query(entities.User
+            ).filter(entities.User.username == username
+            ).filter(entities.User.password == password
+            ).one()
+        return render_template("success.html")
+    except Exception:
+        return render_template("fail.html")"""
+
+@app.route('/messages/<id>', methods = ['GET'])
+def get_message(id):
+    db_session = db.getSession(engine)
+    messages = db_session.query(entities.Message).filter(entities.Message.id == id)
+    for message in messages:
+        js = json.dumps(message, cls=connector.AlchemyEncoder)
+        return  Response(js, status=200, mimetype='application/json')
+
+    message = { 'status': 404, 'message': 'Not Found'}
+    return Response(message, status=404, mimetype='application/json')
+
+@app.route('/create_test_messages', methods = ['GET'])
+def create_test_message():
+    db_session = db.getSession(engine)
+    message = entities.Message(content="Hola", sent_on="20/10/2019", user_from_id="1", user_to_id="2", user_from="Alonso", user_to="Carlos")
+    db_session.add(message)
+    db_session.commit()
+    return "Test message created!"
+
+#Create
+@app.route('/messages', methods = ['POST'])
+def create_message():
+    c = json.loads(request.form['values'])
+    message = entities.Message(
+        content=c['content'],
+        sent_on=c['sent_on'],
+        user_from_id=c['user_from_id'],
+        user_to_id=c['user_to_id'],
+        user_from=c['user_from'],
+        user_to=c['user_to']
+    )
+    session = db.getSession(engine)
+    session.add(message)
+    session.commit()
+    return 'Created Message'
+
+#Read
+@app.route('/messages', methods = ['GET'])
+def get_messages():
+    session = db.getSession(engine)
+    dbResponse = session.query(entities.Message)
+    data = dbResponse[:]
+    return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
+
+#Update
+@app.route('/messages', methods = ['PUT'])
+def update_message():
+    session = db.getSession(engine)
+    id = request.form['key']
+    message = session.query(entities.User).filter(entities.Message.id == id).first()
+    c = json.loads(request.form['values'])
+    for key in c.keys():
+        setattr(message, key, c[key])
+    session.add(message)
+    session.commit()
+    return 'Updated Message'
+
+#Delete
+@app.route('/messages', methods = ['DELETE'])
+def delete_message():
+    id = request.form['key']
+    session = db.getSession(engine)
+    messages = session.query(entities.Message).filter(entities.Message.id == id)
     for message in messages:
         session.delete(message)
     session.commit()
